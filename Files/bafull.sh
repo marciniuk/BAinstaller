bdb31115cbb3904dbb66b47f05f516de5bd22d0a
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2018-11-28 06:04:54 +0100

    dokładniejsze przenoszenie configów

--- Files/bafull.sh
+++ Files/bafull.sh
@@ -102,7 +102,7 @@
 		fi
 
 	yay
-	yes | LC_ALL=en_US.UTF-8 sudo pacman --needed -S i3-gaps compton termite thunar pamixer papirus-icon-theme telegram-desktop rofi nitrogen ttf-font-awesome ttf-dejavu neovim zsh polkit-gnome qutebrowser unclutter xcape pacman-contrib gsimplecal gnome-font-viewer zsh-syntax-highlighting neofetch figlet lolcat xorg-server xdg-user-dirs lightdm-gtk-greeter
+	yes | LC_ALL=en_US.UTF-8 sudo pacman --needed -S i3-gaps python-pip compton termite thunar pamixer papirus-icon-theme telegram-desktop rofi nitrogen ttf-font-awesome ttf-dejavu neovim zsh polkit-gnome qutebrowser unclutter xcape pacman-contrib gsimplecal gnome-font-viewer zsh-syntax-highlighting neofetch figlet lolcat xorg-server xdg-user-dirs lightdm-gtk-greeter
 	clear
 
 ##################
@@ -143,11 +143,11 @@
 			exit 0
 			clear
 		fi
-
+	# tapeta
 	sudo mkdir /home/wallpaper && sudo chown -R "$USER" /home/wallpaper
 		cp ~/blackarrow/blackarrow/wallpaper.jpg /home/wallpaper
+	#stare
 	sudo mkdir ~/OldC
-		sudo mv ~/.config ~/OldC
 		sudo mv ~/.scripts ~/OldC
 		sudo mv ~/.profile ~/OldC
 		sudo mv ~/.xprofile ~/OldC
@@ -155,40 +155,63 @@
 		sudo mv ~/.termite ~/OldC
 		sudo mv ~/.zshrc ~/OldC
 		sudo mv ~/.antigen ~/OldC
+			#.config
+			sudo mv ~/.config/i3/ ~/OldC
+			sudo mv ~/.config/polybar/ ~/OldC
+			sudo mv ~/.config/termite/ ~/OldC
+			sudo mv ~/.config/rofi/ ~/OldC
+			sudo mv ~/.config/ranger/ ~/OldC
+			sudo mv ~/.config/neofetch/ ~/OldC
+			sudo mv ~/.config/gtk-3.0/ ~/OldC
+			sudo mv ~/.config/nitrogen/ ~/OldC
+			sudo mv ~/.config/compton.conf ~/OldC
+			sudo mv ~/.config/nvim/ ~/OldC
+	#przenoszenie
 	if cd ~/blackarrow/blackarrow/ &> /dev/null; then echo ""; else echo "Fail: cd ~/blackarrow/blackarrow/"; fi
-	sudo cp .profile ~/
-	sudo cp .xprofile ~/
-	sudo cp .Xresources ~/
-	sudo cp .zshrc ~/
-	sudo cp .gtkrc-2.0 ~/
-	sudo cp -r ~/blackarrow/blackarrow/.scripts ~/
-	sudo cp -r ~/blackarrow/blackarrow/.themes ~/
-	sudo cp -r ~/blackarrow/blackarrow/themes /usr/share/
-	sudo cp -r ~/blackarrow/blackarrow/backgrounds /usr/share/
-	sudo cp -r ~/blackarrow/blackarrow/lightdm /etc 
-	sudo cp -r ~/blackarrow/blackarrow/.config ~/
-		sudo mkdir ~/.config/nvim/bundle && cd ~/.config/nvim/bundle && sudo chown -R "$USER" ~/.config
-			git clone https://github.com/Shougo/deoplete.nvim
-			git clone https://github.com/junegunn/goyo.vim
-			git clone https://github.com/PotatoesMaster/i3-vim-syntax
-			git clone https://github.com/itchyny/lightline.vim
-			git clone https://github.com/rafaqz/ranger.vim
-			git clone https://github.com/romainl/vim-cool
-			git clone https://github.com/octol/vim-cpp-enhanced-highlight
-			git clone https://github.com/ap/vim-css-color
-			git clone https://github.com/itchyny/vim-gitbranch
-			git clone https://github.com/reedes/vim-pencil
-			git clone https://github.com/tpope/vim-speeddating
-			cp ~/.config/lightline-biual ~/.config/nvim/bundle
+		sudo cp .profile ~/
+		sudo cp .xprofile ~/
+		sudo cp .Xresources ~/
+		sudo cp .zshrc ~/
+		sudo cp .gtkrc-2.0 ~/
+		sudo cp -r ~/blackarrow/blackarrow/.scripts ~/
+		sudo cp -r ~/blackarrow/blackarrow/.themes ~/
+		sudo cp -r ~/blackarrow/blackarrow/themes /usr/share/
+		sudo cp -r ~/blackarrow/blackarrow/backgrounds /usr/share/
+		sudo cp -r ~/blackarrow/blackarrow/lightdm /etc 
+			#.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/i3/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/polybar/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/termite/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/rofi/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/ranger/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/neofetch/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/gtk-3.0/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/nitrogen/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/compton.conf ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/nvim ~/.config
+				sudo mkdir ~/.config/nvim/bundle && cd ~/.config/nvim/bundle && sudo chown -R "$USER" ~/.config
+					git clone https://github.com/Shougo/deoplete.nvim
+					git clone https://github.com/junegunn/goyo.vim
+					git clone https://github.com/PotatoesMaster/i3-vim-syntax
+					git clone https://github.com/itchyny/lightline.vim
+					git clone https://github.com/rafaqz/ranger.vim
+					git clone https://github.com/romainl/vim-cool
+					git clone https://github.com/octol/vim-cpp-enhanced-highlight
+					git clone https://github.com/ap/vim-css-color
+					git clone https://github.com/itchyny/vim-gitbranch
+					git clone https://github.com/reedes/vim-pencil
+					git clone https://github.com/tpope/vim-speeddating
+					cp -r ~/blackarrow/blackarrow/.config/lightline-biual ~/.config/nvim/bundle
+	#przyznanie uprawnień
 	if cd ~/ &> /dev/null; then echo ""; else echo "Fail: cd ~/"; fi
-	sudo chown -R "$USER" ~/OldC
-	sudo chown "$USER" ~/.profile
-	sudo chown "$USER" ~/.xprofile
-	sudo chown "$USER" ~/.Xresources
-	sudo chown "$USER" ~/.zshrc
-	sudo chown -R "$USER" ~/.scripts
-	sudo chown -R "$USER" ~/.themes
-	sudo chown "$USER" ~/.gtkrc-2.0
+		sudo chown -R "$USER" ~/OldC
+		sudo chown "$USER" ~/.profile
+		sudo chown "$USER" ~/.xprofile
+		sudo chown "$USER" ~/.Xresources
+		sudo chown "$USER" ~/.zshrc
+		sudo chown -R "$USER" ~/.scripts
+		sudo chown -R "$USER" ~/.themes
+		sudo chown "$USER" ~/.gtkrc-2.0
 	clear
 
 ###################################
