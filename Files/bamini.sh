8ba5a4bdc81f0579d45f02a5b63f7b8894fbd27b
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2018-12-15 01:12:57 +0100

    bamini is already working

--- Files/bamini.sh
+++ Files/bamini.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # ____    _      __  __ _       _ 
 #| __ )  / \    |  \/  (_)_ __ (_)
 #|  _ \ / _ \   | |\/| | | '_ \| |
@@ -5,200 +6,238 @@
 #|____/_/   \_\ |_|  |_|_|_| |_|_|
 #
 
-#!!!!!!!!!!!!!!!!!!!!#
-# Czasowe wyłączenie #
-#!!!!!!!!!!!!!!!!!!!!#
-
-echo "The ~bamini~ installer has been temporarily disabled due to errors. 
-Please use ~bafull~ or wait for the repair of this installer (I don't know 
-when it will happen - I focus on the development of the full version)."
-exit 0
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#####################
-# Color definitions #
-#####################
+##################### @@@@@@@@@@@@@@@@@@@@@@@@
+# Color definitions # @ Definiowanie kolorów @
+##################### @@@@@@@@@@@@@@@@@@@@@@@@
 
 	if [ -x "$(command -v tput)" ]; then
-		# Foreground
-			bold="$(tput bold)"
-			black="$(tput setaf 0)"
-			green="$(tput setaf 2)"
-			yellow="$(tput setaf 3)"
-			blue="$(tput setaf 4)"
-			reset="$(tput sgr0)"
-		# Background
-			red_bg="$(tput setab 1)"
-			blue_bg="$(tput setab 4)"
+	# Foreground
+		bold="$(tput bold)"
+		black="$(tput setaf 0)"
+		blue="$(tput setaf 4)"
+		reset="$(tput sgr0)"
+	# Background
+		blue_bg="$(tput setab 4)"
 	fi
 
 ######################################################
 
-###############
-# Przywitanie #
-###############
+########### @@@@@@@@@@@@@@@
+# Welcome # @ Przywitanie @
+########### @@@@@@@@@@@@@@@
 
 	clear
-	echo "${black}${blue_bg} BlackArrow installer 1/6 - Welcome in BlackArrow Installer ${reset}
+	echo "${black}${blue_bg}${bold} BlackArrow installer 1/6 - Welcome in BlackArrow Installer ${reset}
 	"
-	echo "	 ____  _            _        _                           
-	| __ )| | __ _  ___| | __   / \   _ __ _ __ _____      __
-	|  _ \| |/ _\` |/ __| |/ /  / _ \ | '__| '__/ _ \ \ /\ / /
-	| |_) | | (_| | (__|   <  / ___ \| |  | | | (_) \ V  V / 
-	|____/|_|\__,_|\___|_|\_\/_/   \_\_|  |_|  \___/ \_/\_/  
-	"
+	echo "${blue}	 ____  _            _        _                           
+	| __ )| | __ _  ___| | __   / \\   _ __ _ __ _____      __
+	|  _ \\| |/ _\` |/ __| |/ /  / _ \\ | '__| '__/ _ \\ \\ /\\ / /
+	| |_) | | (_| | (__|   <  / ___ \\| |  | | | (_) \\ V  V / 
+	|____/|_|\\__,_|\\___|_|\\_\\/_/   \\_\\_|  |_|  \\___/ \\_/\\_/  
+	${reset}"
 	echo "	Welcome to the installer of my Arch Linux configuration. I hope you will like it!
 
 	!!! Installer intended for clean (empty) system installations! If you already have your configuration files,
 	make a copy of them or cancel BlackArrow installations !!!
 	"
-		#zap
-		read -p "Are you sure you want to continue? <y/N> " prompt
+		#FAQ
+		read -r -p "Are you sure you want to continue? <y/N> " prompt
 		if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 		then
 			echo Ok! Get Ready!
 		else
-			clear
 			exit 0
+			clear
 		fi
-	cd ~/
+	
+	if cd ~/ &> /dev/null; then echo ""; else echo "Fail: cd ~/"; fi
 	git clone https://gitlab.com/xXBlackMaskXx/blackarrow
-	cd
 	clear
 
-##########################################################
-# Checking if AUR Helper is installed (sprawdzanie yay?) #
-##########################################################
+############################################# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Checking if AUR Helper is installed (yay) # @ Sprawdzanie czy jest "yay" @
+############################################# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-	echo "${black}${blue_bg} BlackArrow installer 1,5/6 - Installing AUR Helper ${reset}
+	echo "${black}${blue_bg}${bold} BlackArrow installer 1,5/6 - Installing AUR Helper ${reset}
 	"
-	echo "We will check if you have "yay""
+	echo "We will check if you have ~yay~"
 
-		#zap
-		read -p "Are you sure you want to continue? <y/N> " prompt
+		#FAQ
+		read -r -p "Are you sure you want to continue? <y/N> " prompt
 		if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 		then
 			echo "Ok! Get Ready!"
 		else
-			clear
 			exit 0
+			clear
 		fi
 
 	if yay --version &> /dev/null; then
-	  echo "AUR Helper - Yay is installed"
+		echo "AUR Helper - ~Yay~ is installed"
 	else
-	  git clone https://aur.archlinux.org/yay.git
-	  cd yay || exit
-	  makepkg -si
-	  cd ..
-	  rm -rf yay
+		git clone https://aur.archlinux.org/yay.git
+		cd yay || exit
+		makepkg -si
+		cd ..
+		rm -rf yay
 	fi
 	clear
-###################
-# Programy z repo #
-###################
 
-	echo "${black}${blue_bg} BlackArrow installer 2/6 - Installing from repository ${reset}
+######################################### @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Programs from the official repository # @ Programy z oficjalnego repo @
+######################################### @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+	echo "${black}${blue_bg}${bold} BlackArrow installer 2/6 - Installing from repository ${reset}
 	"
 	echo "Now we will install the necessary programs"
 
-		#zap
-		read -p "Are you sure you want to continue? <y/N> " prompt
+		#FAQ
+		read -r -p "Are you sure you want to continue? <y/N> " prompt
 		if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 		then
 			echo "Ok! Get Ready!"
 		else
-			clear
 			exit 0
+			clear
 		fi
 
 	yay
-	sudo pacman -S i3-gaps compton termite pamixer rofi nitrogen ttf-font-awesome ttf-dejavu neovim zsh polkit-gnome qutebrowser unclutter xcape
+	yes | LC_ALL=en_US.UTF-8 sudo pacman --needed -S i3-gaps wireless_tools python-pip compton termite thunar pamixer papirus-icon-theme rofi ttf-dejavu neovim zsh polkit-gnome unclutter xcape pacman-contrib gsimplecal zsh-syntax-highlighting neofetch xorg-server xdg-user-dirs lightdm-gtk-greeter
 	clear
 
-##################
-# Programy z AUR #
-##################
+##################### @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Programs from AUR # @ Programy z AUR (repo użytkowników) @
+##################### @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-	echo "${black}${blue_bg} BlackArrow installer 3/6 - Installing from AUR ${reset}
+	echo "${black}${blue_bg}${bold} BlackArrow installer 3/6 - Installing from AUR ${reset}
 	"
 	echo "Other necessary programs :D "
 
-		#zap
-		read -p "Are you sure you want to continue? <y/N> " prompt
+		#FAQ
+		read -r -p "Are you sure you want to continue? <y/N> " prompt
 		if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 		then
 			echo "Ok! Get Ready!"
 		else
-			clear
 			exit 0
+			clear
 		fi
 
-	yay -S pacman-contrib gsimplecal gnome-font-viewer zsh-syntax-highlighting light i3lock-fancy-git neofetch lolcat figlet ttf-dejavu-sans-mono-powerline-git polybar antigen-git lightdm-gtk-greeter xorg-server xdg-user-dirs 
+	yes | LC_ALL=en_US.UTF-8 yay --needed -S zsh-syntax-highlighting light i3lock-fancy-git ttf-dejavu-sans-mono-powerline-git polybar antigen-git 
 	clear
 
-###################################
-# Komendy po instalacji programów #
-###################################
+################ @@@@@@@@@@@@@@@@@@@@@@@
+# Moving files # @ Przenoszenie plików @
+################ @@@@@@@@@@@@@@@@@@@@@@@
 
-	echo "${black}${blue_bg} BlackArrow installer 4/6 - Starting programs ${reset}
+	echo "${black}${blue_bg}${bold} BlackArrow installer 4/6 - Moving configs ${reset}
+	"
+	echo "And finally transferring configuration files! Required root password (used ~sudo~ command). You can interrupt and move configurations manually. The commands you need are at the very bottom of the blackarrow.sh file"
+
+		#FAQ
+		read -r -p "Are you sure you want to continue? <y/N> " prompt
+		if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+		then
+			echo "Ok! Get Ready!"
+		else
+			exit 0
+			clear
+		fi
+	#Old @Stare
+	sudo mkdir ~/OldC
+		sudo mv ~/.scripts ~/OldC
+		sudo mv ~/.profile ~/OldC
+		sudo mv ~/.xprofile ~/OldC
+		sudo mv ~/.Xresources ~/OldC
+		sudo mv ~/.termite ~/OldC
+		sudo mv ~/.zshrc ~/OldC
+		sudo mv ~/.antigen ~/OldC
+			#.config
+			sudo mv ~/.config/i3/ ~/OldC
+			sudo mv ~/.config/polybar/ ~/OldC
+			sudo mv ~/.config/termite/ ~/OldC
+			sudo mv ~/.config/rofi/ ~/OldC
+			sudo mv ~/.config/ranger/ ~/OldC
+			sudo mv ~/.config/neofetch/ ~/OldC
+			sudo mv ~/.config/gtk-3.0/ ~/OldC
+			sudo mv ~/.config/compton.conf ~/OldC
+			sudo mv ~/.config/nvim/ ~/OldC
+	#Moving @Przenoszenie
+	if cd ~/blackarrow/blackarrow/ &> /dev/null; then echo ""; else echo "Fail: cd ~/blackarrow/blackarrow/"; fi
+		sudo cp .profile ~/
+		sudo cp .xprofile ~/
+		sudo cp .Xresources ~/
+		sudo cp .zshrc ~/
+		sudo cp -r ~/blackarrow/blackarrow/.scripts ~/
+		sudo cp -r ~/blackarrow/blackarrow/backgrounds /usr/share/
+		sudo cp -r ~/blackarrow/blackarrow/lightdm /etc 
+		sudo cp -r ~/blackarrow/blackarrow/fonts/* ~/.local/share/fonts/
+			#.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/i3/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/polybar/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/termite/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/rofi/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/ranger/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/neofetch/ ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/compton.conf ~/.config
+			sudo cp -r ~/blackarrow/blackarrow/.config/nvim ~/.config
+				sudo mkdir ~/.config/nvim/bundle && cd ~/.config/nvim/bundle && sudo chown -R "$USER" ~/.config
+					git clone https://github.com/Shougo/deoplete.nvim
+					git clone https://github.com/junegunn/goyo.vim
+					git clone https://github.com/PotatoesMaster/i3-vim-syntax
+					git clone https://github.com/itchyny/lightline.vim
+					git clone https://github.com/rafaqz/ranger.vim
+					git clone https://github.com/romainl/vim-cool
+					git clone https://github.com/octol/vim-cpp-enhanced-highlight
+					git clone https://github.com/ap/vim-css-color
+					git clone https://github.com/itchyny/vim-gitbranch
+					git clone https://github.com/reedes/vim-pencil
+					git clone https://github.com/tpope/vim-speeddating
+					cp -r ~/blackarrow/blackarrow/.config/lightline-biual ~/.config/nvim/bundle
+	#Granting permissions @Przyznawanie uprawnień
+	if cd ~/ &> /dev/null; then echo ""; else echo "Fail: cd ~/"; fi
+		sudo chown -R "$USER" ~/OldC
+		sudo chown "$USER" ~/.profile
+		sudo chown "$USER" ~/.xprofile
+		sudo chown "$USER" ~/.Xresources
+		sudo chown "$USER" ~/.zshrc
+		sudo chown -R "$USER" ~/.scripts
+	clear
+
+############################### @@@@@@@@@@@@@@@@@@@@@@@@@
+# Commands after installation # @ Komendy po instalacji @
+############################### @@@@@@@@@@@@@@@@@@@@@@@@@
+
+	echo "${black}${blue_bg}${bold} BlackArrow installer 5/6 - Configuring programs ${reset}
 	"
 	echo "Now we will launch the programs"
 
-		#zap
-		read -p "Are you sure you want to continue? <y/N> " prompt
+		#FAQ
+		read -r -p "Are you sure you want to continue? <y/N> " prompt
 		if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 		then
 			echo "Ok! Get Ready!"
 		else
-			clear
 			exit 0
+			clear
 		fi
 
 	systemctl enable lightdm.service
 	xdg-user-dirs-update
+	chsh -s /usr/bin/zsh
+	mkdir ~/.yay-log
+	mkdir "$(xdg-user-dir PICTURES)"/ScreenShots
+	sudo pip3 install neovim
 	clear
 
-#######################
-# Przenoszenie plików #
-#######################
+########## @@@@@@@@@@@@@@@
+# Ending # @ Zakończenie @
+########## @@@@@@@@@@@@@@@
 
-	echo "${black}${blue_bg} BlackArrow installer 5/6 - Moving configs ${reset}
-	"
-	echo "And finally transferring configuration files! Required root password (used "sudo" command). You can interrupt and move configurations manually. The commands you need are at the very bottom of the blackarrow.sh file"
-
-		#zap
-		read -p "Are you sure you want to continue? <y/N> " prompt
-		if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
-		then
-			echo "Ok! Get Ready!"
-		else
-			clear
-			exit 0
-		fi
-
-	cd ~/
-	sudo mkdir ~/OldC&S
-	sudo mv ~/.config ~/OldC&S
-	sudo mv ~/.scripts ~/OldC&S
-	cd ~/blackarrow/blackarrow/
-	sudo cp .profile ~/
-	sudo cp .xprofile ~/
-	sudo cp .Xresources ~/
-	sudo cp .zshrc ~/
-	sudo cp -r ~/blackarrow/blackarrow/.scripts ~/
-	sudo cp -r ~/blackarrow/blackarrow/.config ~/
-	clear
-
-##########
-# Koniec #
-##########
-
-	echo "${black}${blue_bg} BlackArrow installer 6/6 - Ending ${reset}
+	echo "${black}${blue_bg}${bold} BlackArrow installer 6/6 - Ending ${reset}
 	"
 
-	echo "	
+	echo "${bold}
 	                      ##
 	                     ####
 	                    ######
@@ -206,10 +245,10 @@ exit 0
 	                  ##########
 	                 ############
 	                ##############          ____  _            _        _
-	               ################        | __ )| | __ _  ___| | __   / \   _ __ _ __ _____      __
-	              ##################       |  _ \| |/ _\` |/ __| |/ /  / _ \ | '__| '__/ _ \ \ /\ / /
-	             ####################      | |_) | | (_| | (__|   <  / ___ \| |  | | | (_) \ V  V /
-	            ######################     |____/|_|\__,_|\___|_|\_\/_/   \_\_|  |_|  \___/ \_/\_/
+	               ################        | __ )| | __ _  ___| | __   / \\   _ __ _ __ _____      __
+	              ##################       |  _ \\| |/ _\` |/ __| |/ /  / _ \\ | '__| '__/ _ \\ \\ /\\ / /
+	             ####################      | |_) | | (_| | (__|   <  / ___ \\| |  | | | (_) \\ V  V /
+	            ######################     |____/|_|\\__,_|\\___|_|\\_\\/_/   \\_\\_|  |_|  \\___/ \\_/\\_/
 	           ##########    ##########
 	          ##########      ##########
 	         ###########      ###########
@@ -218,32 +257,23 @@ exit 0
 	      ####                          ####
 	     ###                              ###
 	    #                                    #
-	"
+	${reset}"
 
-	read -p "Thanks for using my installer! I'm hoping that you'll like my job! But... are you sure you want to leave??? <y>" prompt
+	read -r -p "Thanks for using my installer! I'm hoping that you'll like my job! But... are you sure you want to leave??? <y>" prompt
 	if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 	then
 	        clear
 		exit 0
 	else
-		#next
-	        figlet "Oooooooo..."
-	        read -p "I love you too but you have to go! <y> " prompt
-	        if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
-	        then
-	                exit 0
-	        else
-	                figlet "Get the fuck out!!!"
-	                exit 0
-	        fi
-		#koniec
+		clear
+		exit 0
 	fi
 
-#  _     _                  ___                       _             _            #
-# | |__ | |___      __     |_ _|  _   _ ___  ___     / \   _ __ ___| |__        ###
-# | '_ \| __\ \ /\ / /      | |  | | | / __|/ _ \   / _ \ | '__/ __| '_ \      #####
-# | |_) | |_ \ V  V / _ _   | |  | |_| \__ \  __/  / ___ \| | | (__| | | |    ### ###
-# |_.__/ \__| \_/\_(_|_|_) |___|  \__,_|___/\___| /_/   \_\_|  \___|_| |_|   ###   ###
+#  _     _                  ___                       _             _           #
+# | |__ | |___      __     |_ _|  _   _ ___  ___     / \   _ __ ___| |__       ###
+# | '_ \| __\ \ /\ / /      | |  | | | / __|/ _ \   / _ \ | '__/ __| '_ \     #####
+# | |_) | |_ \ V  V / _ _   | |  | |_| \__ \  __/  / ___ \| | | (__| | | |   ### ###
+# |_.__/ \__| \_/\_(_|_|_) |___|  \__,_|___/\___| /_/   \_\_|  \___|_| |_|  ###   ###
 #
 # __  __           _        _           
 #|  \/  | __ _  __| | ___  | |__  _   _   _
