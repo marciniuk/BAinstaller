4036662badd643128865beb98496e6b8af92b33d
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2018-12-22 18:31:32 +0100

    add checking the updates and small fix

--- Files/BA-Update.sh
+++ Files/BA-Update.sh
@@ -58,6 +58,16 @@
 		clear
 	fi																							
 
+####################### @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Checking the update # @ Sprawdzanie aktualizacji @
+####################### @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+	yes | LC_ALL=en_US.UTF-8 yay
+	
+	yes | LC_ALL=en_US.UTF-8 sudo pacman --needed -S i3-gaps noto-fonts-emoji dunst wireless_tools python-pip compton termite thunar 			   pamixer papirus-icon-theme telegram-desktop rofi nitrogen ttf-dejavu neovim zsh polkit-gnome unclutter xcape pacman-contrib 					  gsimplecal gnome-font-viewer zsh-syntax-highlighting neofetch figlet lolcat xorg-server xdg-user-dirs lightdm-gtk-greeter 					 |& tee -a ~/yay-log\ "$Data".log
+	
+	yes | LC_ALL=en_US.UTF-8 yay --needed -S zsh-syntax-highlighting light i3lock-fancy-git google-chrome 										   ttf-dejavu-sans-mono-powerline-git polybar antigen-git |& tee -a ~/yay-log\ "$Data".log
+	
 ############################################### @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # Creating folders and downloading new files  # @ Tworzenie folderów oraz pobieranie nowych plików @
 ############################################### @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
