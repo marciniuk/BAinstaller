422f359dc8dd3c3791448cf9f0aa41bf6f3dea81
xXBlackMaskXx <marciniuk.adrian123@gmail.com>
2019-09-21 20:12:13 +0200

    new version "BA-Update"

--- Files/BA-Update-v2.sh
+++ Files/BA-Update-v2.sh
@@ -64,9 +64,9 @@
 
 	yes | LC_ALL=en_US.UTF-8 yay
 
-	yes | LC_ALL=en_US.UTF-8 sudo pacman --needed -S i3-gaps noto-fonts-emoji dunst wireless_tools python-pip compton termite thunar 			   pamixer papirus-icon-theme telegram-desktop rofi nitrogen ttf-dejavu neovim zsh polkit-gnome unclutter xcape pacman-contrib 					  gsimplecal gnome-font-viewer zsh-syntax-highlighting neofetch figlet lolcat xorg-server xdg-user-dirs lightdm-gtk-greeter 					 |& tee -a ~/yay-log\ "$Data".log
+	yes | LC_ALL=en_US.UTF-8 sudo pacman --needed -S wget xclip kirigami2 xorg-xrdb xwallpaper htop lxappearance sddm qt5ct qt5-styleplugins xdotool noto-fonts-cjk sxiv mpv maim light i3-gaps xss-lock noto-fonts-emoji dunst wireless_tools python-pip compton termite thunar pamixer papirus-icon-theme telegram-desktop rofi ttf-dejavu neovim zsh polkit-gnome unclutter xcape pacman-contrib gsimplecal gnome-font-viewer zsh-syntax-highlighting neofetch figlet lolcat xorg-server xdg-user-dirs |& tee -a ~/yay-log\ "$Data".log
 
-	yes | LC_ALL=en_US.UTF-8 yay --needed -S zsh-syntax-highlighting light i3lock-fancy-git google-chrome 										   ttf-dejavu-sans-mono-powerline-git polybar antigen-git |& tee -a ~/yay-log\ "$Data".log
+	yes | LC_ALL=en_US.UTF-8 yay --needed -S color-picker zsh-syntax-highlighting sxiv-rifle ttf-symbola i3lock google-chrome ttf-dejavu-sans-mono-powerline-git polybar antigen-git |& tee -a ~/yay-log\ "$Data".log
 
 ############################################### @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # Creating folders and downloading new files  # @ Tworzenie folderów oraz pobieranie nowych plików @
@@ -94,7 +94,11 @@
 		if sudo mv ~/.termite ~/OldC 2> /dev/null; then echo ""; else echo ".termite is empty"; fi
 		if sudo mv ~/.zshrc ~/OldC 2> /dev/null; then echo ""; else echo ".zshrc is empty"; fi
 		if sudo mv ~/.antigen ~/OldC 2> /dev/null; then echo ""; else echo ".antigen is empty"; fi
+		if sudo mv /usr/lib/sddm ~/OldC 2> /dev/null; then echo ""; else echo "sddm is empty"; fi
+		if sudo mv /usr/share/sddm/themes/chili/ ~/OldC 2> /dev/null; then echo ""; else echo "sddm/themes/chili is empty"; fi
 			#.config
+			if sudo mv ~/.config/qt5ct ~/OldC 2> /dev/null; then echo ""; else echo ".config/qt5ct is empty"; fi
+			if sudo mv ~/.config/dunst ~/OldC 2> /dev/null; then echo ""; else echo ".config/dunst is empty"; fi
 			if sudo mv ~/.config/dmenu ~/OldC 2> /dev/null; then echo ""; else echo ".config/dmenu is empty"; fi
 			if sudo mv ~/.config/gsimplecal/ ~/OldC 2> /dev/null; then echo ""; else echo ".config/gsimplecal/ is empty"; fi
 			if sudo mv ~/.config/i3/ ~/OldC 2> /dev/null; then echo ""; else echo ".config/i3/ is empty"; fi
@@ -104,9 +108,11 @@
 			if sudo mv ~/.config/ranger/ ~/OldC 2> /dev/null; then echo ""; else echo ".config/ranger/ is empty"; fi
 			if sudo mv ~/.config/neofetch/ ~/OldC 2> /dev/null; then echo ""; else echo ".config/neofetch/ is empty"; fi
 			if sudo mv ~/.config/gtk-3.0/ ~/OldC 2> /dev/null; then echo ""; else echo ".config/gtk-3.0/ is empty"; fi
-			if sudo mv ~/.config/nitrogen/ ~/OldC 2> /dev/null; then echo ""; else echo ".config/nitrogen/ is empty"; fi
 			if sudo mv ~/.config/compton.conf ~/OldC 2> /dev/null; then echo ""; else echo ".config/compton.conf is empty"; fi
 			if sudo mv ~/.config/nvim/ ~/OldC 2> /dev/null; then echo ""; else echo ".config/nvim/ is empty"; fi
+			if sudo mv /etc/systemd/logind.conf ~/OldC 2> /dev/null; then echo ""; else echo "/etc/systemd/logind.conf is empty"; fi
+			if sudo mv /etc/X11/xorg.conf.d/30-touchpad.conf ~/OldC 2> /dev/null; then echo ""; else echo "30-touchpad.conf is empty"; fi
+			if sudo mv /usr/bin/i3lock-fancy ~/OldC 2> /dev/null; then echo ""; else echo "/usr/bin/i3lock-fancy"; fi
 	#Moving @Przenoszenie
 	if cd ~/blackarrow2/blackarrow/ 2> /dev/null; then echo ""; else echo "Fail: cd ~/blackarrow/blackarrow/"; fi
 		sudo cp .profile ~/
@@ -121,6 +127,8 @@
 		sudo cp -r ~/blackarrow2/blackarrow/lightdm /etc
 		sudo cp -r ~/blackarrow2/blackarrow/fonts/* ~/.local/share/fonts/
 			#.config
+			sudo cp -r ~/blackarrow2/blackarrow/.config/qt5ct ~/.config
+			sudo cp -r ~/blackarrow2/blackarrow/.config/dunst ~/.config
 			sudo cp -r ~/blackarrow2/blackarrow/.config/dmenu ~/.config
 			sudo cp -r ~/blackarrow2/blackarrow/.config/gsimplecal ~/.config
 			sudo cp -r ~/blackarrow2/blackarrow/.config/i3/ ~/.config
@@ -130,10 +138,13 @@
 			sudo cp -r ~/blackarrow2/blackarrow/.config/ranger/ ~/.config
 			sudo cp -r ~/blackarrow2/blackarrow/.config/neofetch/ ~/.config
 			sudo cp -r ~/blackarrow2/blackarrow/.config/gtk-3.0/ ~/.config
-			sudo cp -r ~/blackarrow2/blackarrow/.config/nitrogen/ ~/.config
 			sudo cp -r ~/blackarrow2/blackarrow/.config/compton.conf ~/.config
 			sudo cp -r ~/blackarrow2/blackarrow/.config/nvim ~/.config
+			sudo cp ~/blackarrow2/blackarrow/logind.conf/ /etc/systemd/
+			sudo cp ~/blackarrow2/blackarrow/30-touchpad.conf/ /etc/X11/xorg.conf.d/
+			sudo cp ~/blackarrow2/blackarrow/i3lock-fancy/ /usr/bin/i3lock-fancy/
 				sudo mkdir ~/.config/nvim/bundle && cd ~/.config/nvim/bundle && sudo chown -R "$USER" ~/.config
+					git clone https://github.com/tpope/vim-commentary
 					git clone https://github.com/Shougo/deoplete.nvim
 					git clone https://github.com/junegunn/goyo.vim
 					git clone https://github.com/PotatoesMaster/i3-vim-syntax
@@ -145,6 +156,7 @@
 					git clone https://github.com/itchyny/vim-gitbranch
 					git clone https://github.com/reedes/vim-pencil
 					git clone https://github.com/tpope/vim-speeddating
+					git clone https://github.com/bronson/vim-trailing-whitespace
 					cp -r ~/blackarrow2/blackarrow/.config/lightline-biual ~/.config/nvim/bundle
 	#Granting permissions @Przyznawanie uprawnień
 	if cd ~/ &> /dev/null; then echo ""; else echo "Fail: cd ~/"; fi
@@ -156,6 +168,7 @@
 		sudo chown -R "$USER" ~/.scripts
 		sudo chown -R "$USER" ~/.themes
 		sudo chown "$USER" ~/.gtkrc-2.0
+		sudo chown -R "$USER" /usr/share/backgrounds
 	clear
 
 ########## @@@@@@@@@@@@@@@
@@ -190,7 +203,7 @@
 	read -r -p "Thanks for using BAUpdate! Have fun using it! <y>" prompt
 	if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 	then
-	        clear
+		clear
 		exit 0
 	else
 		clear
