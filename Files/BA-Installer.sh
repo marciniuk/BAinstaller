40c037fe2132fd1d613db2938c05636685583d2d
xXBlackMaskXx <marciniuk.adrian123@gmail.com>
2019-11-05 23:13:50 +0100

    add pamixer

--- Files/BA-Installer.sh
+++ Files/BA-Installer.sh
@@ -109,7 +109,7 @@
 		#FAQ
 
 	yay
-	sudo pacman --needed -S wget xclip kirigami2 xorg-xrdb xwallpaper htop lxappearance sddm qt5ct qt5-styleplugins xdotool noto-fonts-cjk sxiv mpv maim light i3-gaps xss-lock noto-fonts-emoji dunst wireless_tools python-pip compton termite thunar pamixer papirus-icon-theme telegram-desktop rofi ttf-dejavu neovim zsh polkit-gnome unclutter xcape pacman-contrib gsimplecal gnome-font-viewer zsh-syntax-highlighting neofetch figlet lolcat xorg-server xdg-user-dirs |& tee -a ~/yay-log\ "$Data".log
+	sudo pacman --needed -S pamixer wget xclip kirigami2 xorg-xrdb xwallpaper htop lxappearance sddm qt5ct qt5-styleplugins xdotool noto-fonts-cjk sxiv mpv maim light i3-gaps xss-lock noto-fonts-emoji dunst wireless_tools python-pip compton termite thunar pamixer papirus-icon-theme telegram-desktop rofi ttf-dejavu neovim zsh polkit-gnome unclutter xcape pacman-contrib gsimplecal gnome-font-viewer zsh-syntax-highlighting neofetch figlet lolcat xorg-server xdg-user-dirs |& tee -a ~/yay-log\ "$Data".log
 	clear
 
 
